1969ee27d7a97a3fed8107c8abfe006e6a28ebf3
John Carter <johncarter352@gmail.com>
2020-04-18 03:12:12 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,11 +13,10 @@ class Artist
     song.artist = self
   end
   
-  def add_song_by_name(name)
-    song = Song.new(name)
+  def add_song_by_name(title)
+    song = Song.new(title)
     add_song(song)
   end
-  
   
   
   
