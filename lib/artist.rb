1060de2c1eb8bc661ec316421422dd74bd0f777c
John Carter <johncarter352@gmail.com>
2020-04-18 03:00:59 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,7 +13,7 @@ class Artist
     song.artist = self
   end
   
-  def add_song_by_name(title)
+  def add_song_by_name(title = "Rolling in the Deep")
     song = Song.new(title)
     add_song(song)
   end
